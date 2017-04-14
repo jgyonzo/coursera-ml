f3c41b2fd79d9c17ab53dfc31fd062b4d2b9dcf0
jgyonzo <juan.yonzo@mercadolibre.com>
2017-04-14 19:41:38 -0300

    week 2 little fixes post-DONE

--- machine-learning-ex1/ex1/ex1.m
+++ machine-learning-ex1/ex1/ex1.m
@@ -89,12 +89,10 @@ legend('Training data', 'Linear regression')
 hold off % don't overlay any more plots on this figure
 
 % Predict values for population sizes of 35,000 and 70,000
-predict1 = [1, 3.5] *theta;
-fprintf('For population = 35,000, we predict a profit of %f\n',...
-    predict1*10000);
+predict1 = [1, 3.5] * theta;
+fprintf('For population = 35,000, we predict a profit of %f ...\n', predict1*10000);
 predict2 = [1, 7] * theta;
-fprintf('For population = 70,000, we predict a profit of %f\n',...
-    predict2*10000);
+fprintf('For population = 70,000, we predict a profit of %f ...\n', predict2*10000);
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
@@ -124,12 +122,12 @@ J_vals = J_vals';
 % Surface plot
 figure;
 surf(theta0_vals, theta1_vals, J_vals)
-xlabel('\theta_0'); ylabel('\theta_1');
+xlabel('theta_0'); ylabel('theta_1');
 
 % Contour plot
 figure;
 % Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
 contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
-xlabel('\theta_0'); ylabel('\theta_1');
+xlabel('theta_0'); ylabel('theta_1');
 hold on;
 plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
